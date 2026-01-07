b1828ae32976c850dbfae32895ccd40dfa5b8392
Jacob Costen <32058792+oculometric@users.noreply.github.com>
2026-01-07 14:09:21 +0000

    got some widgets rendering

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@ int main()
     window3->setPosition({ 50, 2 });
     window3->setSize({ 60, 16 });
     
+    window3->setRoot(
+        new RTUI::VerticalBox({
+            new RTUI::Label("i didnt want your little bitchass anyway!~"),
+            new RTUI::Label("busy woman"),
+            new RTUI::Label("but if you need my love"),
+            new RTUI::Label("my clothes are off im coming over to your place"),
+        }));
+    
     while (true)
         comp.update();
 }
--- src/rtui.h
+++ src/rtui.h
@@ -6,8 +6,22 @@
 #include "rtui.h"
 #include "vec2.h"
 
+// TODO: organise into files with context, widget, window, compositor in one, subcompositors in another, subwidgets in another
+
 namespace RTUI
 {
+
+static inline Vec2 clip(Vec2 min_size, Vec2 max_size, Vec2 available_size)
+{
+    int x = 0;
+    if (max_size.x == -1) x = available_size.x;
+    else x = std::min(available_size.x, max_size.x);
+    int y = 0;
+    if (max_size.y == -1) y = available_size.y;
+    else y = std::min(available_size.y, max_size.y);
+    
+    return Vec2{ x, y };
+}
 
 class Context
 {
@@ -16,6 +30,7 @@ class Context
 private:
     std::vector<char> backing;
     int pitch = 0;
+    std::vector<Box2> bounds_stack;
     Box2 permitted_bounds;
     
 public:
@@ -32,42 +47,86 @@ public:
     std::vector<char>::const_iterator begin() const;
     std::vector<char>::const_iterator end() const;
     
+    // TODO: context should handle clipping (for scrollable regions)
+    // TODO: make this so that widgets can't just pop and push their own bounds....
+    void pushPermittedBounds(Vec2 min, Vec2 max);
+    void popPermittedBounds();
+    
 private:
     inline Context() : Context({1, 1}, ' ') { }
     Context(Vec2 size, char fill_value);
     ~Context() = default;
     
-    void setPermittedBounds(Vec2 min, Vec2 max);
     void resize(Vec2 new_size, char fill_value);
 }; 
 
+// TODO: proper dirty flags for the entire tree
+#pragma region WIDGETS
+
 class Widget
 {
-    Transform2 transform;
-    // TODO: transform, z layer, visible, focusable
 public:
-    virtual void render(Context& ctx) const;
+    Transform2 transform; // stores position within parent, and size
+protected:
+    std::vector<Widget*> children;
+    Widget* parent = nullptr;
+    bool visible = false;
     
-    virtual Vec2 getMinSize() const;
-    virtual Vec2 getMaxSize() const;
+public:
+    Widget() = default;
+    // TODO: move and copy constructors?
+    virtual ~Widget() = default;
+    
+    virtual void arrange(Vec2 available_area) { }
+    virtual void render(Context& ctx) const { }
+    
+    virtual Vec2 getMinSize() const { return Vec2{ 1, 1 }; }
+    virtual Vec2 getMaxSize() const { return Vec2{ -1, -1 }; }
 };
 
-// TODO: label
+class Label : public Widget
+{
+public:
+    std::string text;
+    
+public:
+    Label(const std::string& _text) : text(_text) { }
+    
+    void arrange(Vec2 available_area) override { transform.size = clip(getMinSize(), getMaxSize(), available_area); }
+    void render(Context& ctx) const override { ctx.drawText(Vec2{ 0, 0 }, text); }
+    
+    Vec2 getMinSize() const override { return Vec2{ 0, 1 }; }
+    Vec2 getMaxSize() const override { return Vec2{ -1, 1 }; }
+};
+
+class VerticalBox : public Widget
+{
+public:
+    
+public:
+    VerticalBox(const std::vector<Widget*>& _children) { children = _children; }
+    
+    void arrange(Vec2 available_area) override;
+    void render(Context& ctx) const override;
+};
+
 // TODO: button
 // TODO: bitmap image
 // TODO: list view
 // TODO: toggle button
 // TODO: radio button
 // TODO: border
-// TODO: arrangement.... (vertical, horizontal, grid boxes)
 // TODO: tab view
+// TODO: arrangement.... (vertical, horizontal, grid boxes)
+
+#pragma endregion
 
 class Window
 {
     friend class Compositor;
 private:
+    Widget* root = nullptr;
     // TODO: manages widgets, and draws them (controls and state management)
-    // widget transform tree
     
     Vec2 size = { 10, 10 };
     Vec2 position = { 5, 5 };
@@ -83,15 +142,16 @@ public:
     void operator=(const Window& other) = delete;
     void operator=(Window&& other) = delete;
     
-    inline Vec2 getSize() const { return size; }
-    inline void setSize(Vec2 _size) { size = _size; is_dirty = true; }
-    inline Vec2 getPosition() const { return position; }
-    inline void setPosition(Vec2 _position) { position = _position; is_dirty = true; }
-    inline std::string getTitle() const { return title; }
-    inline void setTitle(const std::string& _title) { title = _title; is_dirty = true; }
-    inline void setAllowsMinimise(bool _allows_minimise) { allows_minimise = _allows_minimise; if (!allows_minimise) is_minimised = false; is_dirty = true; }
-    inline void setBorderless(bool _borderless) { borderless = _borderless; is_dirty = true; }
-    inline bool getMinimised() const { return is_minimised; }
+    Vec2 getSize() const { return size; }
+    void setSize(Vec2 _size) { size = _size; is_dirty = true; }
+    Vec2 getPosition() const { return position; }
+    void setPosition(Vec2 _position) { position = _position; is_dirty = true; }
+    std::string getTitle() const { return title; }
+    void setTitle(const std::string& _title) { title = _title; is_dirty = true; }
+    void setAllowsMinimise(bool _allows_minimise) { allows_minimise = _allows_minimise; if (!allows_minimise) is_minimised = false; is_dirty = true; }
+    void setBorderless(bool _borderless) { borderless = _borderless; is_dirty = true; }
+    bool getMinimised() const { return is_minimised; }
+    void setRoot(Widget* _root) { root = _root; is_dirty = true; }
     
     void render(Context& ctx) const;
     
